55ecd622cea72e6b5b05b1589512d55a66c55205
Thanakrit Paithun <supichaya.sit@mail.kmutt.ac.th>
2020-10-20 15:12:27 +0700

    First ver.

---
+++ src/gittest/GitTest.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gittest;
+
+/**
+ *
+ * @author Student
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Portable Git");
+    }
+    public static void rabbit(){
+        System.out.println("(\\_/)");
+        System.out.println("(*.*)");
+        System.out.println("/>o<");
+    }
+}
